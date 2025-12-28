dc409f437f205541a6b040067e7cf315c9454d10
GoyalParas <parasgoyal180803@gmail.com>
2025-12-29 01:31:06 +0530

    error resolved

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { appPlans } from '../assets/assets';
-import Footer from '../components/Footer';
+import Footer from '@/components/Footer';
 import { authClient } from '@/lib/auth-client';
 import { toast } from 'sonner';
 import api from '@/configs/axios';
