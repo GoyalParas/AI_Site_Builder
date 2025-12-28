6209acf935c85d564059a6856aa43064913bb91a
GoyalParas <parasgoyal180803@gmail.com>
2025-12-29 02:26:18 +0530

    retry

--- client/src/pages/Community.tsx
+++ client/src/pages/Community.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import type { Project } from '../types';
 import { Loader2Icon } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
-import Footer from '@/components/Footer.tsx';
+import Footer from '@/components/Footer';
 import api from '@/configs/axios';
 import { toast } from 'sonner';
 
