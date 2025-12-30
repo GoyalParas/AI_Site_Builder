e99a472070b6a7ec9eb6a4a341b8ec48a9f130cc
GoyalParas <parasgoyal180803@gmail.com>
2025-12-30 22:28:48 +0530

    ts error resolve

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express'
 import prisma from '../lib/prisma.js';
 import openai from '../configs/openai.js';
-import type { Version } from '../generated/prisma/client';
+import type { Version } from '../generated/prisma/client.js';
 
 // Controller Function to Make Revision
 export const makeRevision = async (req: Request, res: Response) => {
